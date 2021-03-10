f4e23716662715d6fd3fd852ee1a3f9bbfd85ba6
Wesley <wesaboo@gmail.com>
2021-03-09 18:38:05 -0800

    adds solved folders for week 5 and 6

--- 06-Server-Side-APIs/01-Activities/02-Response/Solved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/02-Response/Solved/assets/js/script.js
@@ -2,21 +2,17 @@
 fetch(
   'https://api.giphy.com/v1/gifs/random?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN'
 )
-  // Converts the response to JSON
+  // Convert the response to JSON
   .then(function(response) {
     return response.json();
   })
   .then(function(response) {
     // Use 'querySelector' to get the ID of where the GIF will be displayed
-    // YOUR CODE HERE
-    //
+    var responseContainerEl = document.querySelector('#response-container');
     // Create an '<img>' element
-    // YOUR CODE HERE
-    //
+    var gifImg = document.createElement('img');
     // Set that element's 'src' attribute to the 'image_url' from our Giphy API response
-    // YOUR CODE HERE
-    //
+    gifImg.setAttribute('src', response.data.image_url);
     // Append the '<img>' element to the page
-    // YOUR CODE HERE
-    //
+    responseContainerEl.appendChild(gifImg);
   });
