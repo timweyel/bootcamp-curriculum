527baca04be0c1fb2a9d4dcddd230a703944c456
emmanuel <emmanueljucaban@gmail.com>
2021-03-09 17:19:38 -0800

    Push week 7

--- 06-Server-Side-APIs/01-Activities/01-Fetch/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/01-Fetch/Unsolved/assets/js/script.js
@@ -1,2 +1,24 @@
 // Write a fetch request to the Giphy API
 // Then log the response in the console
+//API
+// Application Programming Interface
+// How we interact with someone else's application.
+// When we make a request to someone else's API
+// We are requesting DATA from THEIR server so that we can use it
+// in our own application
+
+
+// HTTP request
+// GET requests for data, POST creates data, PUT updates data, DELETE deletes data
+fetch('https://api.giphy.com/v1/gifs/trending?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN')
+  .then(res => res.json())
+  .then(res => {
+    console.log(res)
+  })
+
+
+// fetch('https://api.giphy.com/v1/gifs/trending?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN')
+//   .then(function(res) {
+//     console.log(res);
+//     return res.json();
+//   })
--- 06-Server-Side-APIs/01-Activities/02-Response/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/02-Response/Unsolved/assets/js/script.js
@@ -1,16 +1,23 @@
 // Fetch request to Giphy API random endpoint
 fetch(
-  'https://api.giphy.com/v1/gifs/random?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN'
+  'https://api.giphy.com/v1/gifs/random?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN&tag=taco'
 )
   // Converts the response to JSON
   .then(function(response) {
     return response.json();
   })
   .then(function(response) {
+    console.log(response.data);
     // Use 'querySelector' to get the ID of where the GIF will be displayed
+    const $imgContainer = document.querySelector('#response-container');
+    console.log(response.data.image_url);
     // YOUR CODE HERE
     //
     // Create an '<img>' element
+    const $img = document.createElement('img');
+    $img.setAttribute('src', response.data.image_url)
+    // console.log($img);
+    $imgContainer.appendChild($img);
     // YOUR CODE HERE
     //
     // Set that element's 'src' attribute to the 'image_url' from our Giphy API response
