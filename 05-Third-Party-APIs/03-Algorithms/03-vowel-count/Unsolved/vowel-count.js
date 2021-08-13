f4b36d3ce384b64c6bef6cc2491e3c536fffc193
TimWeyel <tweyel@gmail.com>
2021-08-13 10:48:08 -0700

    vowel-count

--- 05-Third-Party-APIs/03-Algorithms/03-vowel-count/Unsolved/vowel-count.js
+++ 05-Third-Party-APIs/03-Algorithms/03-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,20 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function(str) {
+  // declare a count var
+  // create a vowels array
+  //iterate through array and see if value at each index is a vowelCount
+    //if it is an vowel, increment a vowelCount variaable
+  
+  let count = 0;
+  let vowels = [ 'a', 'e', 'i', 'o', 'u'];
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i].toLowerCase();
+
+    if (vowels.indexOf(char) != -1) {
+      count++;
+    }
+  }
+  return count;
+};
