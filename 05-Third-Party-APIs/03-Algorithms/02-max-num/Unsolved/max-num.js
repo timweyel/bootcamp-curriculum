57d4893e3d61fcc6cda4f42a1860a9f5a2c49023
TimWeyel <tweyel@gmail.com>
2021-08-13 10:29:07 -0700

    max-num

--- 05-Third-Party-APIs/03-Algorithms/02-max-num/Unsolved/max-num.js
+++ 05-Third-Party-APIs/03-Algorithms/02-max-num/Unsolved/max-num.js
@@ -1,3 +1,16 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function(arr) {
+  let largest = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    let currentNum = arr[i];
+
+    if (currentNum > largest) {
+      largest =   currentNum;
+    }
+  }
+  return largest;
+};
+var arr = [3, 1, 17, 5, 6];
+console.log(maxNum(arr));
