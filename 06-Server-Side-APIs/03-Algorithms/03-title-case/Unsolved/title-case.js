7da7edde2fa3f74cbd246dfc6b17f62d989c5c56
TimWeyel <tweyel@gmail.com>
2021-08-16 10:10:41 -0700

    titleCase

--- 06-Server-Side-APIs/03-Algorithms/03-title-case/Unsolved/title-case.js
+++ 06-Server-Side-APIs/03-Algorithms/03-title-case/Unsolved/title-case.js
@@ -1,3 +1,19 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
-var titleCase = function(str) {};
+var titleCase = function(str) {
+  let result = [];
+
+  let wordsArr = str.split(" ");
+
+  for (let i = 0; i < wordsArr.length; i++) {
+    let word = wordsArr[i].split("");
+
+    word[0] = word[0].toUpperCase();
+
+    result.push(word.join(""));
+  }
+  return result.join(" ");
+};
+
+
+console.log(titleCase("a lannister always pays his debts"))
