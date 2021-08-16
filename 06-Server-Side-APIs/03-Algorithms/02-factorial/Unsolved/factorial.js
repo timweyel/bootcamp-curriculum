cf3885ff1b248bb015448e2b3b04ff89a4754571
TimWeyel <tweyel@gmail.com>
2021-08-16 09:58:27 -0700

    factorial

--- 06-Server-Side-APIs/03-Algorithms/02-factorial/Unsolved/factorial.js
+++ 06-Server-Side-APIs/03-Algorithms/02-factorial/Unsolved/factorial.js
@@ -1,3 +1,10 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var factorial = function(num) {
+  let result = 1;
+
+  for (let i = num; i > 1; i--) {
+    result *= i;
+  }
+  return result;
+};
