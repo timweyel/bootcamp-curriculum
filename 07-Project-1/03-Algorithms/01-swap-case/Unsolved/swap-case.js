4a83a74fbbcaf25eb37d4fc63c35a04bcaff7f75
TimWeyel <tweyel@gmail.com>
2021-08-17 09:45:18 -0700

    swap-case

--- 07-Project-1/03-Algorithms/01-swap-case/Unsolved/swap-case.js
+++ 07-Project-1/03-Algorithms/01-swap-case/Unsolved/swap-case.js
@@ -1,3 +1,17 @@
-// Write code to create a function takes a string and returns the string with all of the letter cases swapped
+// Write code to create a function takes a string and 
+//returns the string with all of the letter cases swapped
 
-var swapCase = function(str) {};
+var swapCase = function(str) {
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    if (char === char.toUpperCase()) {
+      result += char.toLowerCase();
+    } else {
+      result += char.toUpperCase();
+    }
+  }
+  return result;
+};
