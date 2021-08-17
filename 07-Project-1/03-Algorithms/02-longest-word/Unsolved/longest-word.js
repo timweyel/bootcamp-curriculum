3dec41048d82cf5e650daae058d7918146b5b7b9
TimWeyel <tweyel@gmail.com>
2021-08-17 09:50:24 -0700

    longestWord

--- 07-Project-1/03-Algorithms/02-longest-word/Unsolved/longest-word.js
+++ 07-Project-1/03-Algorithms/02-longest-word/Unsolved/longest-word.js
@@ -1,3 +1,17 @@
-// Write code to create a function that accepts a string (`str`) and returns the longest word in the string
+// Write code to create a function that accepts a string 
+//(`str`) and returns the longest word in the string
 
-var longestWord = function(str) {};
+var longestWord = function(str) {
+  let strArr = str.split(' ');
+
+  let result = "";
+
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i].length > result.length) {
+      result = strArr[i];
+    }
+  }
+  return result;
+};
+
+longestWord("i love programming");
