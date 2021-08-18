b8a7d1c2295c9420a113a27f83da40a6e732497f
TimWeyel <tweyel@gmail.com>
2021-08-18 14:07:30 -0700

    acronym-builder

--- 07-Project-1/03-Algorithms/03-acronym-builder/Unsolved/acronym-builder.js
+++ 07-Project-1/03-Algorithms/03-acronym-builder/Unsolved/acronym-builder.js
@@ -1,3 +1,15 @@
-// Write code to create a function that accepts a string and returns an acronym for the given string
+// Write code to create a function that accepts a string 
+//and returns an acronym for the given string
 
-var acronymBuilder = function(str) {};
+var acronymBuilder = function(str) {
+  let strArr = str.split(' ');
+  let acronym = "";
+  
+  for (let i = 0; i < strArr.length; i++) {
+    let word = strArr[i];
+    acronym += word[0].toUpperCase();
+  }
+  return acronym;
+};
+
+acronymBuilder("For your information");
