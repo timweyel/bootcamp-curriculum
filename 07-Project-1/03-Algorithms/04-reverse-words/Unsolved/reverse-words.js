8ed627951971e4ee64965e43aa038f99844bceb4
TimWeyel <tweyel@gmail.com>
2021-08-18 14:17:47 -0700

    reversedWords

--- 07-Project-1/03-Algorithms/04-reverse-words/Unsolved/reverse-words.js
+++ 07-Project-1/03-Algorithms/04-reverse-words/Unsolved/reverse-words.js
@@ -1,3 +1,11 @@
 // Write code to create a function that accepts a string (`str`) and reverses the order of the words in the string
 
-var reverseWords = function(str) {};
+var reverseWords = function(str) {
+  let strArr = str.split(' ');
+  let reversedWords = strArr.reverse();
+  return reversedWords.join(' ');
+   
+
+};
+
+console.log(reverseWords("just keep swimming"))
