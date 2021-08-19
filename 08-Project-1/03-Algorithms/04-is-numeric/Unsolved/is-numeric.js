9b408480daeb4c1fadd9ba41ba75a801b4533a40
TimWeyel <tweyel@gmail.com>
2021-08-19 12:15:23 -0700

    is-numeric

--- 08-Project-1/03-Algorithms/04-is-numeric/Unsolved/is-numeric.js
+++ 08-Project-1/03-Algorithms/04-is-numeric/Unsolved/is-numeric.js
@@ -1,3 +1,12 @@
-// Write code to create a function that accepts a string and determines if the string is a valid number. You may not use any regex, built-in methods type conversion to accomplish this.
+// Write code to create a function that accepts a string and 
+//determines if the string is a valid number. You may not use 
+//any regex, built-in methods type conversion to accomplish this.
 
-var isNumeric = function(str) {};
+var str = "-0123.00";
+// var str = "+-a13";
+
+var isNumeric = function(str) {
+  if (str === '') {
+    return false
+  } else return !isNaN(str)
+};
