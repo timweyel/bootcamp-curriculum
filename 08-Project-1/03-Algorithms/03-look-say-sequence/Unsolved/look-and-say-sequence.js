9194e7d4c9d78e198514ed1fb955c15229ba0560
TimWeyel <tweyel@gmail.com>
2021-08-19 12:05:53 -0700

    look-and-say-sequence

--- 08-Project-1/03-Algorithms/03-look-say-sequence/Unsolved/look-and-say-sequence.js
+++ 08-Project-1/03-Algorithms/03-look-say-sequence/Unsolved/look-and-say-sequence.js
@@ -1,5 +1,28 @@
 // Write code to create a function that accepts a number (`n`) and returns a new number that satisfies the look-and-say-sequence
 
-var lookAndSay = function(n) {
+var n = 114333877;
 
+var lookAndSay = function(n) {
+  let result = '';
+  let nArr = (n + ' ').split('');
+  console.log('nArr',nArr);
+  let prevNum = nArr[0];
+  console.log('prevNum',prevNum);
+  let count = 0;
+
+  nArr.forEach(e => {
+    if (e === prevNum) {
+      count ++;
+    }
+    else {
+      result += (count + '') + prevNum;
+      console.log('result', result);
+      prevNum = e;
+      count = 1
+    }
+  });
+  result = parseInt(result);
+  return result;
 };
+
+console.log(lookAndSay(n));
