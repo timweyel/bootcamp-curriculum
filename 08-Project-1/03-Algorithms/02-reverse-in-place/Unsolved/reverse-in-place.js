c43d64a9f7b799139ab9a2ea0db20a45ee5cd40f
TimWeyel <tweyel@gmail.com>
2021-08-19 10:16:04 -0700

    reverse-in-place

--- 08-Project-1/03-Algorithms/02-reverse-in-place/Unsolved/reverse-in-place.js
+++ 08-Project-1/03-Algorithms/02-reverse-in-place/Unsolved/reverse-in-place.js
@@ -1,4 +1,12 @@
-// Write code to return a new string with all of the characters in `str` reversed.
+// Write code to return a new string with all of the characters in 
+//`str` reversed.
 // You may NOT use the built-in reverse method
+var arr = [2, 4, 6, 8]
+var reverseInPlace = function(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    arr.splice(i, 0, arr.pop());
+  }
+  return arr;
+};
 
-var reverseInPlace = function(arr) {};
+console.log(reverseInPlace(arr));
