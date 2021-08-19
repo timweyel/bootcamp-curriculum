74615850832b2adfc1ced0708a47f503b76ab3d9
TimWeyel <tweyel@gmail.com>
2021-08-19 10:01:17 -0700

    reverse-no-built-in

--- 08-Project-1/03-Algorithms/01-reverse-no-built-in/Unsolved/reverse-no-built-in.js
+++ 08-Project-1/03-Algorithms/01-reverse-no-built-in/Unsolved/reverse-no-built-in.js
@@ -1,6 +1,14 @@
-// Write code to return a new string with all of the characters in `str` reversed.
+// Write code to return a new string with all of the characters in 
+//`str` reversed.
 // You may NOT use the built-in reverse method
 
 var reverse = function(str) {
+  let result = '';
 
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+  return result
 };
+
+console.log(reverse('just keep swimming'))
