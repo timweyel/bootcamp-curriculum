66d7794278b489be81e05726618f00030e51a458
TimWeyel <tweyel@gmail.com>
2021-08-20 11:12:22 -0700

    product-of-largest-two

--- 09-NodeJS/03-Algorithms/02-product-of-largest-two/Solved/product-of-largest-two.js
+++ 09-NodeJS/03-Algorithms/02-product-of-largest-two/Solved/product-of-largest-two.js
@@ -1,5 +1,9 @@
 // Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
 
+
+
+
+
 var productOfLargestTwo = function (arr) {
   var largest = null;
   var secondLargest = null;
--- 09-NodeJS/03-Algorithms/02-product-of-largest-two/Unsolved/product-of-largest-two.js
+++ 09-NodeJS/03-Algorithms/02-product-of-largest-two/Unsolved/product-of-largest-two.js
@@ -1,3 +1,24 @@
-// Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
+// Write code to create a function that accepts an array of 
+//numbers, finds the largest two numbers, and returns the product 
+//of the two
 
-var productOfLargestTwo = function(arr) {};
+var arr = [10, 20, 9, 6, 3];
+
+var productOfLargestTwo = function(arr) {
+  let largest = null;
+  let secondLargest = null;
+
+  for (let i = 0; i < arr.length; i++) {
+    let currentNumber = arr[i];
+
+    if (currentNumber > largest || largest === null) {
+      secondLargest = largest;
+      largest = currentNumber
+    } else if (currentNumber > secondLargest || secondLargest === null) {
+      secondLargest = currentNumber
+    }
+  }
+  return largest * secondLargest;
+};
+
+console.log(productOfLargestTwo(arr));
