61d519fbb0fa1451ff012e3ce18f77d068601503
TimWeyel <tweyel@gmail.com>
2021-08-20 11:18:12 -0700

    camel-case

--- 09-NodeJS/03-Algorithms/03-camel-case/Unsolved/camel-case.js
+++ 09-NodeJS/03-Algorithms/03-camel-case/Unsolved/camel-case.js
@@ -1,3 +1,23 @@
-// Write code to create a function that accepts a string and returns the string in camelCase
+// Write code to create a function that accepts a string and returns 
+//the string in camelCase
 
-var camelCase = function(str) {};
+var str = "let the force flow through you";
+
+var camelCase = function(str) {
+  let result = "";
+  let strArr = str.toLowerCase().split(" ");
+  console.log('strArr', strArr);
+
+  for (let i = 0; i < strArr.length; i++) {
+    let word = strArr[i];
+    let letters = word.split("");
+
+    if (i>0) {
+      letters[0] = letters[0].toUpperCase();
+    }
+    result += letters.join("");
+  }
+  return result;
+};
+
+console.log(camelCase(str));
