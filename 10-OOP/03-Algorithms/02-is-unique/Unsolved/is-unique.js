018f7b29610f5d457e0fc1160f5d6657503b1024
TimWeyel <tweyel@gmail.com>
2021-08-23 10:45:59 -0700

    is-unique

--- 10-OOP/03-Algorithms/02-is-unique/Unsolved/is-unique.js
+++ 10-OOP/03-Algorithms/02-is-unique/Unsolved/is-unique.js
@@ -1,4 +1,21 @@
 // Write code to create a function that accepts an array numbers
-// Return `true` is no number appears in the array more than once, else return `false`
+// Return `true` is no number appears in the array more than once, 
+// else return `false`
 
-var isUnique = function(arr) {};
+var arr = [20, -20, 18, 2, 3, 20]; 
+
+var isUnique = function(arr) {
+  let obj = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    if (obj[arr[i]]) {
+      return false;
+    }
+    
+    obj[arr[i]] = true;
+    
+  }
+  return true;
+};
+
+console.log(isUnique(arr));
