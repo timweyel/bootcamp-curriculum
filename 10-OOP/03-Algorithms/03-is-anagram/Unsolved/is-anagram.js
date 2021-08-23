699d2be2c10517f1c97f2fef50ae332cb7ef6dba
TimWeyel <tweyel@gmail.com>
2021-08-23 11:09:22 -0700

    isAnagram

--- 10-OOP/03-Algorithms/03-is-anagram/Unsolved/is-anagram.js
+++ 10-OOP/03-Algorithms/03-is-anagram/Unsolved/is-anagram.js
@@ -1,4 +1,23 @@
 // Write code to create a function that accepts two strings
-// Return true if the strings are anagrams of each other, otherwise return false
+// Return true if the strings are anagrams of each other, 
+// otherwise return false
 
-var isAnagram = function(strA, strB) {};
+// var strA = "nnjjdyus";
+// var strB = "njdnsjyu";
+var strA = "tacocat";
+var strB = "ctatocato";
+
+var isAnagram = function(strA, strB) {
+  if (strA.length !== strB.length) {
+    return false;
+  } else {
+    strA = strA.split('').sort().join('');
+    // console.log(strA);
+    strB = strB.split('').sort().join('');
+    if (strA === strB) {
+      return true;
+    } else return false;
+}
+};
+
+console.log(isAnagram(strA, strB));
