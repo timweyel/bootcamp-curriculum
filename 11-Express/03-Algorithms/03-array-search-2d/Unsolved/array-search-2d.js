1e340ad01bb964584f705f8eca72965174287b4e
TimWeyel <tweyel@gmail.com>
2021-08-24 11:44:48 -0700

    array-search-2d

--- 11-Express/03-Algorithms/03-array-search-2d/Unsolved/array-search-2d.js
+++ 11-Express/03-Algorithms/03-array-search-2d/Unsolved/array-search-2d.js
@@ -2,4 +2,20 @@
 // Each 2D element contains either the string "X" or "O" (both capitalized)
 // Return the number of times "X" appears in the 2D array
 
-var arraySearch2D = function(arr) {};
+var arraySearch2D = function(arr) {
+  //create a variable to keep track of the count of X's
+  //iterate through each array    
+    //if the element === 'X', then increment the count
+
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] === "X") {
+        count++;
+      }
+    }
+  }
+  return count;
+};
+ 
