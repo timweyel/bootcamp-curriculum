cbf6adf8c1264f198cc2ac179220e14a3915d2a7
TimWeyel <tweyel@gmail.com>
2021-08-24 11:29:36 -0700

    most-songs-in-playlist

--- 11-Express/03-Algorithms/01-most-songs-in-playlist/Unsolved/most-songs-in-playlist.js
+++ 11-Express/03-Algorithms/01-most-songs-in-playlist/Unsolved/most-songs-in-playlist.js
@@ -1,20 +1,34 @@
 // Write code to create a function that accepts an array of numbers
 // Each number represents the duration of a song in minutes
-// Return the maximum number of songs from the array that can fit in a 60 minute playlist
+// Return the maximum number of songs from the array that can fit in a 
+// 60 minute playlist
 
-var noMoreThanPrevious = function(str) {
-  var letterCount = {};
+var arr = [3, 4, 7, 2]; // 4 element array
+// var arr = [4, 2, 5, 3, 1, 1, 2, 3, 4, 2, 5, 6, 3, 2, 4, 7, 3, 2, 3];
 
-  var currentLetter = arr[0];
-  var prevLetter;
-  for (var i = 1; i < str.length; i++) {
-    var letter = str[i];
-    letterCount[letter] = (letterCount[letter] || 0) + 1;
+var mostSongsInPlaylist = function(arr) {
+  //sort the array
+  //declare a variable that start keeping track of minutes at 0
+  //loop through the array
+    //add each index to the total minutes variable, and increment numSongs
+    //if adding it exceeds 60, then return the total number of songs (lenght)
+    //otherwise keeping going
 
-    if (prevLetter) {
-      if (letterCount[letter] > letterCount[letter]) {
-        return false;
-      }
+  let sortedArr = arr.sort();
+  let totalMinutes = 0;
+  let numSongs = 0;
+
+  for (let i = 0; i < sortedArr.length; i++) {
+    let currentSongDuration = sortedArr[i];
+
+    if (totalMinutes + currentSongDuration <= 60) {
+      totalMinutes += currentSongDuration;
+      numSongs++;
+    } else {
+      return numSongs;
     }
   }
+  return numSongs;
 };
+
+console.log(mostSongsInPlaylist(arr));
