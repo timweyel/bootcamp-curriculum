d40632d2a8a8a475f0a8ed650a4e84fb7f76b0c3
TimWeyel <tweyel@gmail.com>
2021-08-25 10:14:56 -0700

    zeroes-and-ones

--- 12-SQL/03-Algorithms/02-zeroes-and-ones/Unsolved/zeroes-and-ones.js
+++ 12-SQL/03-Algorithms/02-zeroes-and-ones/Unsolved/zeroes-and-ones.js
@@ -1,5 +1,24 @@
-// Write code to create a function that accepts a string containing only 0s and 1s
+// Write code to create a function that accepts a string containing 
+// only 0s and 1s
 // Return true if there are an equal number of 0s and 1s
 // Else return false
 
-var zeroesAndOnes = function(str) {};
+var str = "00110011";
+
+var zeroesAndOnes = function(str) {
+  let zeroes = 0;
+  let ones = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let current = str[i];
+
+    if (str[i] === "0") {
+      zeroes++;
+    } else {
+      ones++;
+    }
+  }
+  return zeroes === ones;
+};
+
+console.log(zeroesAndOnes(str));
