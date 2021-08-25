1c57044f8a761595812da96f15f85bab8034c320
TimWeyel <tweyel@gmail.com>
2021-08-25 10:05:58 -0700

    multiply-into-20

--- 12-SQL/03-Algorithms/01-multiply-into-20/Unsolved/multiply-into-20.js
+++ 12-SQL/03-Algorithms/01-multiply-into-20/Unsolved/multiply-into-20.js
@@ -1,4 +1,28 @@
 // Write code to create a function that accepts an array of unique numbers
-// If any two numbers in the array add up to 20, return true, else return false
+// If any two numbers in the array add up to 20, return true, else return 
+// false
 
-var multiplyInto20 = function(arr) {};
+var arr = [3, 4, 5, 7, 9];
+
+var multiplyInto20 = function(arr) {
+  let obj = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    let currentNumber = arr[i];
+    // console.log('currentNumber',currentNumber)
+
+    let targetNumber = 20 / currentNumber;
+    // console.log('targetNumber',targetNumber)
+
+    if (obj[targetNumber]) {
+
+      return true;
+    } else {
+      obj[currentNumber] = true;
+    }
+  }
+  return false;
+};
+
+
+console.log(multiplyInto20(arr));
