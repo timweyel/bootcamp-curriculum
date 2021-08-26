e3384d3717123cbcd5112860486ce34336c63a9b
TimWeyel <tweyel@gmail.com>
2021-08-26 12:29:36 -0700

    string-map

--- 13-ORM/03-Algorithms/03-string-map/Unsolved/string-map.js
+++ 13-ORM/03-Algorithms/03-string-map/Unsolved/string-map.js
@@ -1,5 +1,24 @@
-// Write code to create a function that accepts a string and returns an object
+// Write code to create a function that accepts a string and 
+// returns an object
 // The object should contain keys for each character in the string
-// Each key should point to an array containing the indexes the character is found in the string
+// Each key should point to an array containing the indexes the 
+// character is found in the string
 
-var stringMap = function(str) {};
+var str = "HTML & CSS";
+
+var stringMap = function(str) {
+  let obj = {};
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    if (obj[char]) {
+      obj[char].push(i);
+    } else {
+      obj[char] = [i];
+    }
+  }
+  return obj;
+};
+
+console.log(stringMap(str));
