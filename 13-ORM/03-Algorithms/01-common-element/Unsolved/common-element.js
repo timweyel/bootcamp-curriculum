277329d88aa2d7aee6c375ce778b927e4ced7c30
TimWeyel <tweyel@gmail.com>
2021-08-26 11:47:39 -0700

    common-element

--- 13-ORM/03-Algorithms/01-common-element/Unsolved/common-element.js
+++ 13-ORM/03-Algorithms/01-common-element/Unsolved/common-element.js
@@ -1,6 +1,28 @@
-// Write code to create a function that accepts two arrays of numbers
+// Write code to create a function that accepts two arrays of 
+// numbers
 // There will be one number common to both arrays
 // Return the common number
 // You may not use the `indexOf` or `includes` method
 
-var commonElement = function(arrA, arrB) {};
+var arrA = [1, 9, 8, 7];
+var arrB = [10, 12, 1, 6, 5];
+
+var commonElement = function(arrA, arrB) {
+  let elements = {};
+
+  for (let i = 0; i < arrA.length; i++) {
+    let num = arrA[i];
+    elements[num] = true;
+  }
+
+  for (let i = 0; i < arrB.length; i++) {
+    let num = arrB[i];
+
+    if (elements[num] === true) {
+      return num;
+    }
+  } 
+  
+};
+
+console.log(commonElement(arrA, arrB));
