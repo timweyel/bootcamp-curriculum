443e63614cc37b21e9b235a1a9c638bccc085c8c
TimWeyel <tweyel@gmail.com>
2021-08-26 12:19:33 -0700

    permutation-substring

--- 13-ORM/03-Algorithms/02-permutation-substring/Unsolved/permutation-substring.js
+++ 13-ORM/03-Algorithms/02-permutation-substring/Unsolved/permutation-substring.js
@@ -2,4 +2,35 @@
 // Return true if the second string is a substring of any permutation of the first string
 // Else return false
 
-var permutationSubstring = function(str, sub) {};
+var str = "smapnuer";
+var sub = "super";
+
+var permutationSubstring = function(str, sub) {
+  let obj = {};
+
+  for (let i = 0; i < sub.length; i++) {
+    let char = sub[i];
+
+    if (char in obj) {
+      obj[char]++;
+    } else {
+      obj[char] = 1;
+    }
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (char in obj) {
+      obj[char] -= 1;
+    }
+  }
+
+  for (let key in obj) {
+    if (obj[key] > 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(permutationSubstring(str, sub));
