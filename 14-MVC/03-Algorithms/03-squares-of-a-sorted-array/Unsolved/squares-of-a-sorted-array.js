9b0c55e65411d58da6cb52eaf527f1618d393aaa
TimWeyel <tweyel@gmail.com>
2021-08-30 11:25:09 -0700

    squares-of-a-sorted-array

--- 14-MVC/03-Algorithms/03-squares-of-a-sorted-array/Unsolved/squares-of-a-sorted-array.js
+++ 14-MVC/03-Algorithms/03-squares-of-a-sorted-array/Unsolved/squares-of-a-sorted-array.js
@@ -1,3 +1,18 @@
-// Write code to create a function that accepts an array of integers sorted in ascending (increasing) order and returns a new array containing the squares of each number in ascending order
+// Write code to create a function that accepts an array of 
+// integers sorted in ascending (increasing) order and returns 
+// a new array containing the squares of each number in ascending 
+// order
 
-var sortedSquares = function(arr) {};
+var arr = [-4, -1, 0, 3, 10];
+
+var sortedSquares = function(arr) {
+  let squaredArr = arr.map(function(e) {
+    return e ** 2;
+  });
+  let sortedSquares = squaredArr.sort(function(a, b) {
+    return a - b;
+  });
+  return sortedSquares;
+};
+
+console.log(sortedSquares(arr));
