52af55d21663069c284ad3e7a8ede99771d64f46
TimWeyel <tweyel@gmail.com>
2021-08-30 10:06:16 -0700

    doubleTripleMap

--- 14-MVC/03-Algorithms/01-double-triple-map/Unsolved/double-triple-map.js
+++ 14-MVC/03-Algorithms/01-double-triple-map/Unsolved/double-triple-map.js
@@ -1,5 +1,33 @@
-// Write code to create a function that accepts an array of numbers and returns a new array that corresponds to the original array
-// If a element in the original array is even, the element at the same index in the new array should be double the original element
-// If an element in the original array is odd, the element at the same index of the new array should be triple the original element
+// Write code to create a function that accepts an array of numbers 
+// and returns a new array that corresponds to the original array
+// If a element in the original array is even, the element at the 
+// same index in the new array should be double the original element
+// If an element in the original array is odd, the element at the 
+//same index of the new array should be triple the original element
 
-var doubleTripleMap = function(arr) {};
+var arr = [1, 2, 3, 4];
+
+// var doubleTripleMap = function(arr) {
+//   let result = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//       result.push(arr[i] * 2)
+//     } else {
+//       result.push(arr[i] * 3)
+//     }
+//   }
+//   return result;
+// };
+
+var doubleTripleMap = function(arr) {
+  let result = arr.map(function(num) {
+    if (num % 2 === 0) {
+      return num * 2;
+    }
+    return num * 3;
+  })
+  return result;
+}
+
+console.log(doubleTripleMap(arr));
