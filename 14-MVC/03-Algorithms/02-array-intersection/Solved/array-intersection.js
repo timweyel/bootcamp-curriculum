714cde32aff9ba33c58620ac1575b1d6d8a1ca25
TimWeyel <tweyel@gmail.com>
2021-08-30 10:45:57 -0700

    array-intersection

--- 14-MVC/03-Algorithms/02-array-intersection/Solved/array-intersection.js
+++ 14-MVC/03-Algorithms/02-array-intersection/Solved/array-intersection.js
@@ -1,6 +1,9 @@
 // Write code to create a function that accepts two arrays of numbers
 // Return a new array containing the intersecting elements of the arrays
 
+
+
+
 var arrayIntersection = function(arr1, arr2) {
   var numMap = {};
   var result = [];
--- 14-MVC/03-Algorithms/02-array-intersection/Unsolved/array-intersection.js
+++ 14-MVC/03-Algorithms/02-array-intersection/Unsolved/array-intersection.js
@@ -1,4 +1,35 @@
 // Write code to create a function that accepts two arrays of numbers
 // Return a new array containing the intersecting elements of the arrays
 
-var arrayIntersection = function(arr1, arr2) {};
+var arr1 = [1, 2, 3, 4];
+var arr2 = [3, 4, 5, 6];
+
+var arrayIntersection = function(arr1, arr2) {
+  let numTracker = {};
+  let result = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    let num = arr1[i];
+
+    if (numTracker[num] === undefined) {
+      numTracker[num] = 1;
+    } else {
+      numTracker[num]++;
+      console.log('numTracker1', numTracker)
+    }
+  }
+
+  for (let i = 0; i < arr2.length; i++) {
+    let num = arr2[i];
+
+    if (numTracker[num]) {
+      result.push(num);
+      // console.log('result',result)
+      numTracker[num]--;
+      // console.log('numTracker2', numTracker);
+    }
+  }
+  return result;
+  };
+
+  console.log(arrayIntersection(arr1, arr2));
