aa6e4d6c82bfd1ef7cb98d8b2944884e24ebe103
TimWeyel <tweyel@gmail.com>
2021-09-03 09:39:34 -0700

    missing-number

--- 16-Project-2/03-Algorithms/01-missing-number/Unsolved/missing-number.js
+++ 16-Project-2/03-Algorithms/01-missing-number/Unsolved/missing-number.js
@@ -1,3 +1,35 @@
 // Write a function that takes an unsorted array of positive and unique integers and returns the number missing from the array
 
-var missingNumber = function(nums) {};
+var arr = [3, 0, 1];
+
+var missingNumber = function(arr) {
+  arr.sort();
+
+  let numbersFound = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    let elem = arr[i];
+
+    numbersFound[elem] = true;
+  }
+
+  for (let i = 0; i < arr.length + 1; i++) {
+    if (!numbersFound[i]) {
+      return i;
+    }
+  }
+};
+
+console.log(missingNumber(arr));
+
+// sort array
+// loop through array
+// check each element to see that 
+
+
+
+// arr = [3, 0, 1];
+// i = 0
+// does arr[i] + 1 === arr[i + 1]
+  // if so, increment i
+  // if not, return arr[i] + 1
