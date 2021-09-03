c29b3cb2eb3b852fee801cfcef750186b0b63a26
TimWeyel <tweyel@gmail.com>
2021-09-03 10:02:02 -0700

    left-rotation

--- 16-Project-2/03-Algorithms/02-left-rotation/Unsolved/left-rotation.js
+++ 16-Project-2/03-Algorithms/02-left-rotation/Unsolved/left-rotation.js
@@ -2,4 +2,23 @@
 // Modify the original array rather than returning a new one
 // There is no need to return from this function
 
-var leftRotation = function(arr, positions) {};
+var arr = [1, 2, 3];
+var positions = 2;
+
+var leftRotation = function(arr, positions) {
+  if (arr.length === 0) {
+    return;
+  }
+
+  while (positions > 0) {
+    let firstElem = arr.shift();
+    console.log('firstElem', firstElem);
+    console.log('arr', arr);
+  
+    arr.push(firstElem);
+
+    positions--;
+  } 
+};
+
+console.log(leftRotation());
